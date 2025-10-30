00c02d573c38fafc622581b91662c4924b140be6
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-10-30 14:51:16 +0500

    added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,16 @@
 import Header from "./Header"
+import SidebarLeft from "./Sidebar/SidebarLeft"
 
 
 function Home() {
   return (
     <>
       <Header/>
+      <SidebarLeft/>
+      <main>
+        <h1>salom</h1>
+      </main>
+
     </>
   )
 }
---
+++ src/Components/Sidebar/SidebarLeft.jsx
@@ -0,0 +1,17 @@
+import './SidebarLeft.css'
+
+function SidebarLeft() {
+  return (
+    <div id='SidebarLeft' >
+    <ul>
+        <li>🏡Asosiy</li>
+        <li>📖Kurslarim</li>
+        <li>🎥Eduverse</li>
+        <li>⌨️MarsCode</li>
+        <li>📰Blog</li>
+    </ul>
+    </div>
+  )
+}
+
+export default SidebarLeft
