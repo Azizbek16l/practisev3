93f8cf9f73449a99ac1594d418fc33e2e504103b
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-11-06 16:16:54 +0500

    added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
       </main> */}
       <Shop/>
       
-    <SidebarRight/>
+    
     </div>
   )
 }
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,19 +1,33 @@
+import { useState,useEffect } from 'react'
 import './Shop.css'
 
 function Shop() {
+    const [products,setproducts]=useState([])
+    useEffect(() => {      
+    fetch('https://fakestoreapi.com/products?limit=34')
+    .then(res=>res.json())   
+    .then(data=>setproducts(data))
+    }, []);
   return (
     <div className='cards'>
-    <div class="card">
+    {
+        products.map((product)=>(
+             <div className="card">
         <div className='card-img'>
             <img src="./product.png" alt="buy card"/>        
         </div>
 
         <div className='card-info'>
-        <h3 className='card-title'>Mars Pen</h3>
-        <p className='card-price'>49🪙 <span>| 13 ta qoldi</span></p>
+        <h3 className='card-title'>{product.title}</h3>
+        <p className='card-price'>{product.price}🪙 <span>| 13 ta qoldi</span></p>
          <button>Sotib olish</button>
         </div>
-    </div>
+    </div> 
+        ))
+    }
+    
+
+   
     </div>
   )
 }
